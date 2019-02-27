9e678e9eddfcc2dce6fdb2183940cbdfda71a694
Peter Paul <peterpaularasu@gmail.com>
2019-02-27 20:03:23 +0530

    shopping app.

---
+++ shopping_app/shopping_app/Controller/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shopping_app
+//
+//  Created by Peter Paul on 27/02/19.
+//  Copyright © 2019 Emilda pvt. ltd. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ shopping_app/shopping_app/Views/textFeild.swift
@@ -0,0 +1,25 @@
+//
+//  Currency.swift
+//  shopping_app
+//
+//  Created by Peter Paul on 27/02/19.
+//  Copyright © 2019 Emilda pvt. ltd. All rights reserved.
+//
+
+import UIKit
+
+class textFeild: UITextField {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.245956206)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder{
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+
+}
