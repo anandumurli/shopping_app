e0e4a9c9905be9c58bccb8007651a7d7d6e9683e
Peter Paul <peterpaularasu@gmail.com>
2019-02-28 20:17:34 +0530

    this is done

--- shopping_app/shopping_app/Views/textFeild.swift
+++ shopping_app/shopping_app/Views/textFeild.swift
@@ -10,6 +10,22 @@ import UIKit
 
 @IBDesignable
 class textFeild: UITextField {
+    
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLbl = UILabel( frame: CGRect(x: 5, y: (frame.size.height / 2) - (size / 2), width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.5924749028, green: 0.5924749028, blue: 0.5924749028, alpha: 0.65)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2696931671, green: 0.2696931671, blue: 0.2696931671, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+        
+    }
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -22,7 +38,7 @@ class textFeild: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.245956206)
         layer.cornerRadius = 5.0
         textAlignment = .center
-        
+        clipsToBounds = true
         if let p = placeholder{
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
             attributedPlaceholder = place
